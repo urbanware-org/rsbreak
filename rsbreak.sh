6a748c93129ed859b74f1454d4a266f4a5a2e6d5
urbanware-org <github@urbanware.org>
2018-08-05 19:11:52 +0200

    Add link to GitLab

--- rsbreak.sh
+++ rsbreak.sh
@@ -6,6 +6,7 @@
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 #
 # GitHub: https://github.com/urbanware-org/rsbreak
+# GitLab: https://gitlab.com/urbanware-org/rsbreak
 # ============================================================================
 
 version="1.0.0"
